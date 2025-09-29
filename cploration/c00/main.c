8b838b55bef0ce6862b7ede01c8ec3f765fb37ac
garcbenn <bgarcia13@cocc.edu>
2025-09-28 19:19:36 -0700

    C-ploration 0: Print Hello World! to stdout

--- cploration/c00/main.c
+++ cploration/c00/main.c
@@ -4,12 +4,9 @@
  * Benny Garcia
  * Fall 2025
  ********************************************/
-
 #include <stdio.h>
 
 int main(void) {
-    // printf() displays the string given
-    printf("Hello world!\n");
-    // main returns 0 if exiting without error
+    fputs("Hello world!\n", stdout);  // explicit stdout
     return 0;
 }
