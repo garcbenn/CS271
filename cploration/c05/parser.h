60501fa5d6f9103788e5cc171f46179ec3d0e2e3
garcbenn <bgarcia13@cocc.edu>
2025-11-02 17:33:01 -0800

    Add parser.c, parser.h, main.c, and ASM test files for C-ploration 5

---
+++ cploration/c05/parser.h
@@ -0,0 +1,28 @@
+/****************************************
+ * C-ploration 5 for CS 271
+ * 
+ * [NAME] Benny Garcia
+ * [TERM] FALL 2025
+ * 
+ ****************************************/
+#ifndef PARSER_H
+#define PARSER_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+#define MAX_LINE_LENGTH  200
+
+/** function prototypes **/
+char *strip(char *s);
+
+void parse(FILE *file);
+
+bool is_Atype (const char *);
+bool is_label (const char *);
+bool is_Ctype (const char *);
+
+#endif
