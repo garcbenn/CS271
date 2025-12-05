ca54826de21894dc10929ab7f98369c2a93db0e6
garcbenn <bgarcia13@cocc.edu>
2025-12-04 17:36:31 -0800

    Copied Cploration 10 source files into Project 6 directory

---
+++ 6/main.c
@@ -0,0 +1,42 @@
+/*
+
+C-ploration 10 for CS 271 
+[NAME] Benny Garcia
+[TERM] FALL 2025
+  
+*/
+
+#include "parser.h"
+#include "error.h"
+#include "symtable.h"
+
+#define MAX_INSTRUCTION_COUNT 30000
+
+int main(int argc, const char *argv[]) {
+
+    if (argc != 2) {
+        exit_program(EXIT_INCORRECT_ARGUMENTS, argv[0]);
+    }
+
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL) {
+        exit_program(EXIT_CANNOT_OPEN_FILE, argv[1]);
+    }
+
+   instruction *instructions = malloc(MAX_INSTRUCTION_COUNT * sizeof(instruction));
+   if(instructions == NULL) {
+        printf("ERROR: Could not allocate memory for instructions.\n");
+        return EXIT_FAILURE;
+   }
+
+   int num_instructions = parse(file, instructions);
+
+    /*symtable_print_labels(); */
+
+    fclose(file);
+    free(instructions);
+
+    return EXIT_SUCCESS;
+}
+
+
